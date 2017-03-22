d23c7110bcd5d1823dd9e66ffaedd07082f4cc47
ananthaprakash <anithiya@cisco.com>
2017-03-21 23:15:03 -0700

    step 5

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+   url: 'https://www.codeschool.com/users/1752939.json',
+   dataType: 'jsonp',
+   success: function(response) {
+     var badges = document.querySelector('#badges');
+     var courses = "";
+     response.courses.completed.forEach(function(el){
+       courses+="<div class='course'><h3 title="+ el.title + ">"+ el.badge +"</h3><a href=\"" + el.url + "\" target=_blank class=\"btn btn-primary\">See Course</a></div>";
+     });
+     badgets.innerHTML = courses;
+   }
+ });
 
 });
