d994e836ce431d38e3f4ed5c52b4935488f99972
ananthaprakash <anithiya@cisco.com>
2017-03-22 00:43:20 -0700

    with comments

--- assets/main.js
+++ assets/main.js
@@ -8,7 +8,18 @@ $(function() {
      response.courses.completed.forEach(function(el){
        var newDiv = document.createElement('div');
        newDiv.className="course";
-       var subhead = document.createElement('h3')
+       var subhead = document.createElement('h3');
+       subhead.textContent = el.title;
+       newDiv.appendChild(subhead);
+       var courseImage = document.createElement('img');
+       courseImage.src = el.badge;
+       newDiv.appendChild(courseImage);
+       var button = document.createElement('a');
+       button.className = "btn btn-primary";
+       button.href= el.url;
+       button.target = "_blank";
+       button.textContent = "See Course";
+       newDiv.appendChild(button);
        badges.appendChild(newDiv);
      });
    }
